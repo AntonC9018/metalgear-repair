546a20f1a048d54bbabf80a7ef57f47dc6872b34
zapto123 <tsoorckanukris@gmail.com>
2020-02-02 12:13:19 +0200

    more tweaks and features

---
+++ Assets/Scripts/MapCamMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamMovement : MonoBehaviour
+{
+    public CharacterController mapCamControl;
+    public MapToggle mapToggle;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mapToggle.isZoomedin) {
+            mapCamControl.Move(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")));
+        }
+    }
+}
--- Assets/Scripts/MapToggle.cs
+++ Assets/Scripts/MapToggle.cs
@@ -7,13 +7,17 @@ public class MapToggle : MonoBehaviour
     // Start is called before the first frame update
     public CamMovement camMove;
     public InputController inputController;
-    bool isZoomedin = true;
+    public bool isZoomedin = true;
+    public Camera playerCamera;
+    public Camera mapCamera;
 
     // Update is called once per frame
 
     void Start ()
     {
         //isZoomedIn = true;
+        playerCamera.enabled = isZoomedin;
+        mapCamera.enabled = !isZoomedin;
     }
 
     void Update()
@@ -21,7 +25,8 @@ public class MapToggle : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.M))
         {
             isZoomedin = !isZoomedin;
-
+            playerCamera.enabled = isZoomedin;
+            mapCamera.enabled = !isZoomedin;
         }
     }
 }
